0b5e47efd7a7fa9e259d804087f5e75a9c6798c0
Isaac de Moraes <30375595+Iakim@users.noreply.github.com>
2021-04-09 12:43:15 -0300

    Update inlabs-auto-download-pdf.py

--- public/python/inlabs-auto-download-pdf.py
+++ public/python/inlabs-auto-download-pdf.py
@@ -3,6 +3,7 @@ import requests
 
 login = "email@dominio.com"
 senha = "sua_senha"
+
 ## Tipos de Diários Oficiais da União permitidos: do1 do2 do3 (Contempla as edições extras) ##
 tipo_dou="do1 do2 do3"
 
@@ -15,34 +16,40 @@ headers = {
     "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
     }
 s = requests.Session()
-response = s.request("POST", url_login, data=payload, headers=headers, verify=False)
 
-if s.cookies.get('inlabs_session_cookie'):
-    cookie = s.cookies.get('inlabs_session_cookie')
-else:
-    print("Falha ao obter cookie. Verifique suas credenciais");
-    exit(37)
+def download():
+    if s.cookies.get('inlabs_session_cookie'):
+        cookie = s.cookies.get('inlabs_session_cookie')
+    else:
+        print("Falha ao obter cookie. Verifique suas credenciais");
+        exit(37)
+    # Montagem da URL:
+    ano = date.today().strftime("%Y")
+    mes = date.today().strftime("%m")
+    dia = date.today().strftime("%d")
+    data_completa = ano + "-" + mes + "-" + dia
+    
+    # Download inicial
+    for dou_secao in tipo_dou.split(' '):
+        print("Aguarde Download...")
+        url_arquivo = url_download + data_completa + "&dl=" + ano + "_" + mes + "_" + dia + "_ASSINADO_" + dou_secao + ".pdf"
+        cabecalho_arquivo = {'Cookie': 'inlabs_session_cookie=' + cookie, 'origem': '736372697074'}
+        response_arquivo = s.request("GET", url_arquivo, headers = cabecalho_arquivo)
+        if response_arquivo.status_code == 200:
+            with open(data_completa + "-" + dou_secao + ".pdf", "wb") as f:
+                f.write(response_arquivo.content)
+                print("Arquivo %s salvo." % (ano + "_" + mes + "_" + dia + "_ASSINADO_" + dou_secao + ".pdf"))
+                del response_arquivo
+                del f
+        elif response_arquivo.status_code == 404:
+                print("Arquivo não encontrado: %s" % (ano + "_" + mes + "_" + dia + "_ASSINADO_" + dou_secao + ".pdf"))
+    print("Aplicação encerrada")
+    exit(0)
 
-# Montagem da URL:
-ano = date.today().strftime("%Y")
-mes = date.today().strftime("%m")
-dia = date.today().strftime("%d")
-data_completa = ano + "-" + mes + "-" + dia
-
-# Download inicial
-for dou_secao in tipo_dou.split(' '):
-    print("Aguarde Download...")
-    url_arquivo = url_download + data_completa + "&dl=" + ano + "_" + mes + "_" + dia + "_ASSINADO_" + dou_secao + ".pdf"
-    cabecalho_arquivo = {'Cookie': 'inlabs_session_cookie=' + cookie, 'origem': '736372697074'}
-    response_arquivo = s.request("GET", url_arquivo, headers = cabecalho_arquivo)
-    if response_arquivo.status_code == 200:
-        with open(data_completa + "-" + dou_secao + ".pdf", "wb") as f:
-    	    f.write(response_arquivo.content)
-    	    print("Arquivo %s salvo." % (ano + "_" + mes + "_" + dia + "_ASSINADO_" + dou_secao + ".pdf"))
-    	    del response_arquivo
-    	    del f
-    elif response_arquivo.status_code == 404:
-		    print("Arquivo não encontrado: %s" % (ano + "_" + mes + "_" + dia + "_ASSINADO_" + dou_secao + ".pdf"))
-
-print("Aplicação encerrada")
-exit(0)
+def login():
+    try:
+        response = s.request("POST", url_login, data=payload, headers=headers)
+        download()
+    except requests.exceptions.ConnectionError:
+        login()
+login()
